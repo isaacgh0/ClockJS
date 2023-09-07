fb680d3a2a3b3b79c816a378a77be887f82d865a
Saky-Dev <garciaisaac468@gmail.com>
2023-09-07 15:22:54 -0600

    feat: added miliseconds functionality
    added miliseconds count with js and interval,
    created animation on stopwatch border

--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -1,16 +1,31 @@
 const playStopwatch = document.getElementById('play-stopwatch')
 const resetStopwatch = document.getElementById('reset-stopwatch')
+const progress = document.querySelector('div.progress')
 const time = document.getElementById('time')
+const mili = document.getElementById('mili')
 const date = new Date()
+
 let interval
 
 const getLocaleTime = numbers => `${numbers.map(num => `0${num}`.slice(-2))}`.replace(',', ':')
 
+const updateMiliseconds = mili => {
+  if (mili > 500) {
+    const px = 10 - Math.floor((mili - 500) / 50)
+    progress.style.boxShadow = `0 0 ${px}px #00CECB`
+  } else {
+    const px = Math.floor(mili / 50)
+    progress.style.boxShadow = `0 0 ${px}px #00CECB`
+  }
+}
+
 const init = () => {
   interval = setInterval(() => {
-    date.setTime(date.getTime() + 1000)
+    date.setTime(date.getTime() + 22)
     time.value = getLocaleTime([date.getMinutes(), date.getSeconds()])
-  }, 1000)
+    mili.innerText = `00${date.getMilliseconds()}`.slice(-3)
+    updateMiliseconds(date.getMilliseconds())
+  }, 22)
 }
 
 const pause = () => {
@@ -24,12 +39,15 @@ const reset = () => {
 
   date.setHours(0, 0, 0, 0)
   date.setMinutes(0, 0, 0, 0)
+  date.setSeconds(0, 0, 0, 0)
   date.setMilliseconds(0, 0, 0, 0)
 
   playStopwatch.firstElementChild.src = 'assets/icons/player-play-filled.svg'
   playStopwatch.lastElementChild.innerHTML = 'Play'
 
   time.value = getLocaleTime([date.getMinutes(), date.getSeconds()])
+  mili.innerText = '000'
+  progress.style.boxShadow = '0 0 0 #00CECB'
 }
 
 playStopwatch.addEventListener('click', e => {
