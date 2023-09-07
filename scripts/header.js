5bbc89933143ca76a4111ebaea0a519eb398481c
Saky-Dev <garciaisaac468@gmail.com>
2023-09-07 07:22:00 -0600

    feat: initialized stopwatch page
    created stopwatch html page, added new icons
    for buttons, generalized classes css tree selectors

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,10 @@
 const buttonNavbarMenu = document.getElementById('navbar-menu')
 const navbar = document.getElementsByTagName('nav')[0]
+const selected = document.querySelector('nav ul li a.selected')
 
 buttonNavbarMenu.addEventListener('click', () => {
   buttonNavbarMenu.classList.toggle('open')
   navbar.className = buttonNavbarMenu.className
 })
+
+selected.addEventListener('click', e => { e.preventDefault() })
