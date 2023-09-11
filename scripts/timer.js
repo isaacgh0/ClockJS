032b5ffdaf1d0d0e6831d104e403c1a51cdcc5d1
Saky-Dev <garciaisaac468@gmail.com>
2023-09-11 09:08:20 -0600

    feat: added functionalit to modal
    input data form in modal is working, it saves times,
    added rules to restrict data type of user can put on input,
    added some animations when dialog is visible

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,10 +1,63 @@
 const time = document.getElementById('time')
 const dialog = document.getElementsByTagName('dialog')[0]
+const cancelDialog = document.getElementById('cancel-dialog')
+const submitDialog = document.getElementById('submit-dialog')
+const errorMessage = document.getElementsByClassName('error-message')[0]
+const inputs = [...dialog.getElementsByTagName('input')]
+const regexNumber = /^[0-9:]+$/
+const regexSpecial = /^[-e:]+$/
+const date = new Date()
+
+inputs.forEach(input => {
+  input.addEventListener('keydown', e => {
+    if (regexSpecial.test(e.key)) {
+      e.preventDefault()
+    }
+
+    if (e.currentTarget.value.length > 1 && (regexNumber.test(e.key) || regexSpecial.test(e.key))) {
+      e.preventDefault()
+    }
+  })
+})
 
 time.addEventListener('click', () => {
+  dialog.classList.add('visible')
   dialog.showModal()
 })
 
 dialog.firstElementChild.addEventListener('submit', e => {
   e.preventDefault()
 })
+
+cancelDialog.addEventListener('click', () => {
+  dialog.classList.remove('visible')
+  dialog.close()
+})
+
+submitDialog.addEventListener('click', () => {
+  const [minutes, seconds] = inputs.map(input => Number(input.value))
+
+  if ((seconds < 1 || seconds > 59) && (minutes < 1 || minutes > 59)) {
+    errorMessage.classList.add('visible')
+    return
+  }
+
+  inputs.forEach(input => {
+    input.value = ''
+  })
+
+  errorMessage.classList.remove('visible')
+
+  date.setMinutes(minutes)
+  date.setSeconds(seconds)
+
+  time.value = `${`0${minutes}`.slice(-2)}:${`0${seconds}`.slice(-2)}`
+
+  dialog.classList.remove('visible')
+  dialog.close()
+})
+
+date.setHours(0)
+date.setMinutes(0)
+date.setSeconds(0)
+date.setMilliseconds(0)
